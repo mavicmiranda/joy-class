aeb76f8621233befe0c72310ff66397fc72dfa5d
mavicmiranda <mavicmiranda99@gmail.com>
2024-10-29 13:12:48 -0300

    atividades att

---
+++ prep-html-css/desenvolvimento-extra1-1.py
@@ -0,0 +1,11 @@
+# Declare dois arrays, cada um com um mínimo de cinco elementos, e imprima eles no terminal usando o comando print(). 
+# O primeiro array deve conter os produtos de uma loja da sua escolha (loja de comida, materiais de construção, música, etc). 
+# O segundo array deve conter os anos de nascimento de familiares e amigos seus. 
+# Lembre-se de usar nomes descritivos para nomear cada variável, e de usar o tipo de dado apropriado para cada lista (strings, booleanos, números inteiros, floats).
+
+jogos_da_steam = ["Devour","Dead by Daylight", "Among Us", "Counter-Strike 2", "Bloons TD 6"]
+
+anos_de_nascimento = ["1999", "2002", "1997", "1964", "1956"]
+
+print("Jogos da Steam:", jogos_da_steam)
+print("Anos de nascimento de familiares e amigos:", anos_de_nascimento)
